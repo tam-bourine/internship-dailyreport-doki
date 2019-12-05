46ba4ada0febb3da4ef218e2961cc29d8434a2fb
Yuki Kitagawa <kitagawa@tam-bourine.co.jp>
2019-12-05 20:42:41 +0900

    add project

---
+++ daily-report-tambourine-backend/docker/postgres/init/2_insert_seed.sql
@@ -0,0 +1,4 @@
+insert into local.users (username, password, email) values
+ ('keid', 'keidpass', 'keid@developer.com'),
+ ('jobs', 'jobspass', 'jobs@developer.com'),
+ ('mask', 'maskpass', 'mask@developer.com');
