6b179ec9225d767ffc9086b2b405a18e8903e1e9
KodaiD <bb_dkoudai@yahoo.co.jp>
2019-12-06 13:56:33 +0900

    post

---
+++ daily-report-tambourine-backend/database/migrations/2019_12_06_030222_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create( 'posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->char("user_id");
+            $table->text("contents");
+
+
+            //$table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
