259704e1cfc4a6b9a88394e787c42a5b57d8b435
KodaiD <bb_dkoudai@yahoo.co.jp>
2019-12-17 14:04:06 +0900

    PostPolicyの作成

---
+++ app/Policies/PostPolicy.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Policies;
+
+use App\Post;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\AuthorizationException;
+
+class PostPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function update(User $user, Post $post)
+    {
+         if ($user->id === $post->user_id) {
+             return true;
+         } else {
+             throw new AuthorizationException('you are not allowed to update');
+         }
+    }
+
+    public function delete(User $user, Post $post)
+    {
+        if ($user->id === $post->user_id) {
+            return true;
+        } else {
+            throw new AuthorizationException('you are not allowed to delete');
+        }
+    }
+
+}
